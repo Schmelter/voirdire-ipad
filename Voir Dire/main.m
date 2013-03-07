64a6024c49d174c648367a7ca4fdd6310608b4d4
Greg Schmelter <greg.schmelter@experianinteractive.com>
2013-03-06 20:53:38 -0700

    Empty project commit

---
+++ Voir Dire/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Voir Dire
+//
+//  Created by Greg Schmelter on 3/6/13.
+//  Copyright (c) 2013 Greg Schmelter. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "VDAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([VDAppDelegate class]));
+    }
+}
