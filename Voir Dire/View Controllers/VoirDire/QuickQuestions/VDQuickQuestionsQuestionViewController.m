ff7d0a341c9228a807d760f7512d7a5c98b2eb0d
Greg Schmelter <greg.schmelter@experianinteractive.com>
2013-04-11 21:49:28 -0600

    Start work on Quick Questions viewing section

--- Voir Dire/View Controllers/VoirDire/QuickQuestions/VDQuickQuestionsQuestionViewController.m
+++ Voir Dire/View Controllers/VoirDire/QuickQuestions/VDQuickQuestionsQuestionViewController.m
@@ -7,8 +7,15 @@
 //
 
 #import "VDQuickQuestionsQuestionViewController.h"
+#import "FXLabel.h"
+#import <QuartzCore/QuartzCore.h>
 
 @interface VDQuickQuestionsQuestionViewController ()
+
+@property (nonatomic, readwrite, weak) IBOutlet FXLabel *questionNum;
+@property (nonatomic, readwrite, weak) IBOutlet FXLabel *questionText;
+
+-(void)updateView;
 
 @end
 
@@ -27,12 +34,28 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view.;
+    
+    [self updateView];
 }
 
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+-(void)viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
+}
+
+-(void)updateView {
+    _questionNum.text = [NSString stringWithFormat:@"%i", _quickQuestion.quickQuestion.quickQuestionID];
+    
+    CGSize questionTextSize = [_quickQuestion.quickQuestion.question sizeWithFont:_questionText.font forWidth:_questionText.frame.size.width lineBreakMode:NSLineBreakByWordWrapping];
+    _questionText.text = _quickQuestion.quickQuestion.question;
+    _questionText.frame = CGRectMake(_questionText.frame.origin.x, _questionText.frame.origin.y, _questionText.frame.size.width, questionTextSize.height);
+    
+    NSLog(@"Question Text Frame: %@", NSStringFromCGRect(_questionText.frame));
+    NSLog(@"Question Text Parent Frame: %@", NSStringFromCGRect([_questionText superview].frame));
+}
+
+-(void)setQuickQuestion:(VDCompoundQuickQuestion *)quickQuestion {
+    _quickQuestion = quickQuestion;
+    [self updateView];
 }
 
 @end
